53ec77a2f62e5c6b65a040ae753bc7bd0670dd0a
Praneeth reddy Kancharakuntla <pkancha2@asu.edu>
2024-07-09 19:30:55 -0700

    Upd

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -12,65 +12,56 @@ const Experience = () => {
                     <h4>Languages</h4>
                     <h4 className='text-light'>
                         <ul>• Java</ul>
-                        <ul>• Python</ul> 
-                        <ul>• C</ul>
-                        <ul>• C++</ul>
                         <ul>• JavaScript</ul>
                         <ul>• C#</ul>
-                    </h4>
-                </div>
-            </article>
-            <article className='experience__details'>
-                <div className='spacing'>
-                    <h4>Database Tech</h4>
-                    <h4 className='text-light'>
-                        <ul>• SQL</ul>
-                        <ul>• NoSQL</ul> 
-                        <ul>• MySQL</ul>
-                        <ul>• PostgreSQL</ul>
-                        <ul>• SQLite</ul>
-                        <ul>• Firebase</ul>
-                        <ul>• DynamoDB</ul>
-                        <ul>• MongoDB</ul>
-                        <ul>• Snowflake</ul>
-                        <ul>• SAP HANA</ul>
-                    </h4>
-                </div>
-            </article>
-            <article className='experience__details'>
-                <div className='spacing'>
-                    <h4>Cloud Tech</h4>
-                    <h4 className='text-light'>
-                        <ul>• AWS</ul>
-                        <ul>• Azure</ul>
-                        <ul>• GCP</ul>
-                    </h4>
-                </div>
-            </article>
-            <article className='experience__details'>
-                <div className='spacing'>
-                    <h4>Frontend Tech</h4>
-                    <h4 className='text-light'>
-                        <ul>• ReactJS</ul>
                         <ul>• HTML</ul>
                         <ul>• CSS</ul>
+                        <ul>• SQL</ul>
+                        <ul>• PL/SQL</ul>
                     </h4>
                 </div>
             </article>
             <article className='experience__details'>
                 <div className='spacing'>
-                    <h4>Other Tech</h4>
+                    <h4>Frameworks</h4>
                     <h4 className='text-light'>
-                        <ul>• Tableau</ul>
+                        <ul>• Angular</ul>
+                        <ul>• React.js</ul>
                         <ul>• Spring Boot</ul>
                         <ul>• Hibernate</ul>
-                        <ul>• Node.js</ul>
-                        <ul>• GIT</ul>
-                        <ul>• Android Studio</ul>
-                        <ul>• OpenAI</ul>
-                        <ul>• Figma</ul>
-                        <ul>• Kubernetes</ul>
+                    </h4>
+                </div>
+            </article>
+            <article className='experience__details'>
+                <div className='spacing'>
+                    <h4>Cloud and Databases</h4>
+                    <h4 className='text-light'>
+                        <ul>• AWS</ul>
+                        <ul>• Docker</ul>
                         <ul>• Jenkins</ul>
+                        <ul>• MySQL</ul>
+                        <ul>• MongoDB</ul>
+                        <ul>• PostgreSQL</ul>
+                        <ul>• Cassandra</ul>
+                    </h4>
+                </div>
+            </article>
+            <article className='experience__details'>
+                <div className='spacing'>
+                    <h4>Tools and Technology</h4>
+                    <h4 className='text-light'>
+                        <ul>• Linux</ul>
+                        <ul>• GitHub</ul>
+                        <ul>• Google Colab</ul>
+                        <ul>• Postman</ul>
+                        <ul>• Swagger</ul>
+                        <ul>• GIT</ul>
+                        <ul>• GitHub Actions</ul>
+                        <ul>• Azure Release Management</ul>
+                        <ul>• Terraform</ul>
+                        <ul>• Kubernetes CLI (Kubectl, Kubectx)</ul>
+                        <ul>• Helm</ul>
+                        <ul>• Kafka</ul>
                     </h4>
                 </div>
             </article>
--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -10,20 +10,20 @@ const Work = () => {
             <h2>My Experience</h2>
             <VerticalTimeline>
                 <VerticalTimelineElement
-                    className="vertical-timeline-element--work "
+                    className="vertical-timeline-element--work"
                     contentStyle={{ background: '#2c2c6c', color: '#fff', borderRadius: '2rem' }}
                     contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
                     date="Aug 2023 - May 2024"
                     iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                    icon = {<TbBrandGithubCopilot />} >
+                    icon={<TbBrandGithubCopilot />} >
                     <h3 className="vertical-timeline-element-title" style={{'color': '#4db5ff'}}>Elevance Health</h3>
                     <h4 className="vertical-timeline-element-subtitle" style={{'color': '#4db5ff'}}>Full Stack Java Developer</h4>
                     <p style={{'color': '#bebebe'}}>
-                    <br/>● Developed and optimized the Appointment Scheduling Module using Java, Spring Boot, and J2EE, enhancing scheduling efficiency by 30% through effective API design and database integrations with MongoDB and SQL Server.
-                    <br/>● Designed and deployed the Insurance Policy Module using Java, Spring Boot, Docker, AWS, Kubernetes, and Jenkins, resulting in a 35% improvement in user interface responsiveness and consistent user experience across platforms.
-
-                    <br/>● Implemented Reactive Forms and Angular Services for seamless API communication with Spring Boot, leading to a 30% improvement in user engagement and scheduling efficiency.
-                    <br/><span style={{'color': '#4db5ff'}}>Technologies: </span> <br/>: JavaSE, JDK 1.8, Hibernate 3.0, Spring, React, AWS, Groovy, Tiles, JSP, HTML, CSS, Angular 4, jQuery, XML, XSLT, SQL Server, Maven, Apache Tomcat 8, Sonar, Eclipse 6.0, SVN, WindowsXP, Jenkins, Springboot, JUnit, MongoDB, Docker</p>
+                        ● Enhanced scheduling efficiency by 30% through effective API design in Spring Boot and database integrations with MongoDB and SQL Server.
+                        <br/>● Improved UI responsiveness by 35% and ensured consistent user experience using Angular and Spring MVC.
+                        <br/>● Increased user engagement by 30% with Reactive Forms and Angular Services for seamless API communication.
+                        <br/><span style={{'color': '#4db5ff'}}>Technologies: </span> Java, Spring Boot, J2EE, Angular, MongoDB, SQL Server, Docker, AWS, Kubernetes, Jenkins
+                    </p>
                 </VerticalTimelineElement>  
 
                 <VerticalTimelineElement
@@ -32,17 +32,17 @@ const Work = () => {
                     contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
                     date="Aug 2020 - June 2022"
                     iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                    icon = {<TbBrandGithubCopilot />}
+                    icon={<TbBrandGithubCopilot />}
                 >
                     <h3 className="vertical-timeline-element-title" style={{'color': '#4db5ff'}}>AT&T</h3>
-                    <h4 className="vertical-timeline-element-subtitle" style={{'color': '#4db5ff'}}> Java/J2EE Developer </h4>
-                    <p style={{'color': '#bebebe'}}>● Designed and developed the Customer Management Module using Java, Spring Boot, and Hibernate, managing customer profiles with MySQL, and built interactive UIs with Angular and SASS for a responsive experience.
-<br/>● Implemented secure authentication with Spring Security, utilizing JWT and OAuth 2.0 for user login and role-based access control, ensuring secure access to customer information.
-<br/>● Architected and deployed the Billing and Payment Module with Java, Spring Boot, and PostgreSQL, leveraging Docker and Kubernetes for microservices, and configured CI/CD pipelines with Jenkins, reducing deployment time by 50% and increasing release frequency by 40%.
-                        <br/><br/><span style={{'color': '#4db5ff'}}>Technologies: </span> <br/> Java, JavaScript, Spring Boot, J2EE, Angular, Spring MVC, MongoDB, SQL Server, Docker, AWS, Kubernetes, Jenkins, Reactive Forms, Angular Services</p>
+                    <h4 className="vertical-timeline-element-subtitle" style={{'color': '#4db5ff'}}>Java/J2EE Developer</h4>
+                    <p style={{'color': '#bebebe'}}>
+                        ● Managed customer profiles with Spring Boot and MySQL, integrating Angular and SASS for interactive UIs.
+                        <br/>● Implemented secure authentication with Spring Security, JWT, and OAuth 2.0 for user login and role-based access.
+                        <br/>● Reduced deployment time by 50% with Docker, Kubernetes for microservices, and Jenkins for CI/CD pipelines.
+                        <br/><span style={{'color': '#4db5ff'}}>Technologies: </span> Java, Spring Boot, Hibernate, Angular, SASS, MySQL, Spring Security, Docker, Kubernetes, Jenkins
+                    </p>
                 </VerticalTimelineElement>  
-
-
 
                 <VerticalTimelineElement
                     className="vertical-timeline-element--work"
@@ -50,18 +50,16 @@ const Work = () => {
                     contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
                     date="June 2018 - July 2020"
                     iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                    icon = {<TbBrandAzure />}
+                    icon={<TbBrandAzure />}
                 >
-                    <h3 className="vertical-timeline-element-title" style={{'color': '#4db5ff'}}> Amrock </h3>
+                    <h3 className="vertical-timeline-element-title" style={{'color': '#4db5ff'}}>Amrock</h3>
                     <h4 className="vertical-timeline-element-subtitle" style={{'color': '#4db5ff'}}>Java Developer</h4>
                     <p style={{'color': '#bebebe'}}>
-                    ●	Developed scalable and maintainable user interfaces using JavaScript, React, and Material-UI, integrated with SASS to optimize
-user experience and maintainability.<br/>●	Architected efficient middleware functions in Node.js and Express.js, enhancing authentication mechanisms and error handling,
-resulting in a 30% reduction in processing time.
-<br/>●	Leveraged Spring MVC to develop RESTful APIs and web applications, optimizing controller mappings and request handling to
-improve response times and application performance. 
-
-                        <br/><br/><span style={{'color': '#4db5ff'}}>Technologies: </span> <br/>Java, JavaScript, Spring Boot, J2EE, Angular, Spring MVC, MongoDB, SQL Server, Docker, AWS, Kubernetes, Jenkins, Reactive Forms, Angular Services</p>
+                        ● Developed scalable UIs with React, Material-UI, and SASS to optimize user experience and maintainability.
+                        <br/>● Enhanced authentication and error handling with Node.js and Express.js, reducing processing time by 30%.
+                        <br/>● Optimized RESTful APIs and web applications using Spring MVC for better performance.
+                        <br/><span style={{'color': '#4db5ff'}}>Technologies: </span> JavaScript, React, Material-UI, SASS, Node.js, Express.js, Spring MVC, Java
+                    </p>
                 </VerticalTimelineElement>  
             </VerticalTimeline>
         </section>
